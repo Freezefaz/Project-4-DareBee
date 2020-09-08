66961b812f2804eb06984230f4abb0cfa5b58ce9
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-08 09:14:02 +0000

    changed secret key designation and created user testcase

--- DareBeeProject/settings.py
+++ DareBeeProject/settings.py
@@ -22,7 +22,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DJANGO_SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- users/tests.py
+++ users/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
+from .models import User
 from django.shortcuts import get_object_or_404
 
 # Create your tests here.
 class UserModelTestCase(TestCase):
 
     def test_create_user_model(self):
-        first_name = "John"
-        last_name = "Doe"
-        contact_number = "90009000"
-        email = "jd@email.com"
-        password = "jd1234"
+        u = User()
+        u.first_name = "John"
+        u.last_name = "Doe"
+        u.contact_number = "90009000"
+        u.email = "jd@email.com"
+        u.password = "jd1234"
+        # u.save()

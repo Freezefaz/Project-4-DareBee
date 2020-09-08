07b92eced1f9067ca4aeffd6bb121f0cddc3df81
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-08 09:27:47 +0000

    test user can be retrieved by id

--- users/tests.py
+++ users/tests.py
@@ -12,4 +12,12 @@ class UserModelTestCase(TestCase):
         u.contact_number = "90009000"
         u.email = "jd@email.com"
         u.password = "jd1234"
-        # u.save()
+        # to save the user
+        u.save()
+        # test if can save by id
+        # check if id is not zero
+        self.assertTrue(u.id > 0)
+        # get user base on model and the primary key
+        saved_user = get_object_or_404(User, pk=u.id)
+        # correct if the user exist
+        self.assertTrue(saved_user is not None)

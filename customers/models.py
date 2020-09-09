401a4973eec93b91255534bb24687f370583c4d5
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 05:53:55 +0000

    created and migrated customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    # columns in the table
+    first_name = models.CharField(blank=False, max_length=255)
+    last_name = models.CharField(blank=False, max_length=255)
+    email = models.CharField(blank=False, max_length=255)
+    password = models.CharField(blank=False, max_length=255)
+
+    # make to string
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name

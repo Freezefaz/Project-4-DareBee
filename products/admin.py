c0100c4c52f76eed977fc2500a3f91816f628a1d
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 04:00:38 +0000

    created and migrated meaplan and mealplan type

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ExerciseType, Exercise
+from .models import ExerciseType, Exercise, MealplanType, Mealplan
 
 # Register your models here.
 admin.site.register(ExerciseType)
 admin.site.register(Exercise)
+admin.site.register(MealplanType)
+admin.site.register(Mealplan)
--- products/models.py
+++ products/models.py
@@ -22,3 +22,25 @@ class Exercise(models.Model):
     # to make string
     def __str__(self):
         return self.title
+
+
+class MealplanType(models.Model):
+    # columns in the table
+    type = models.CharField(blank=False, max_length=100)
+
+    # to make string
+    def __str__(self):
+        return self.type
+
+
+class Mealplan(models.Model):
+    # columns in the table
+    title = models.CharField(blank=False, max_length=255)
+    description = models.CharField(blank=False, max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=3, blank=False)
+    mealplan_type = models.ForeignKey(MealplanType, on_delete=models.CASCADE)
+
+    # to make string
+    def __str__(self):
+        return self.title
+

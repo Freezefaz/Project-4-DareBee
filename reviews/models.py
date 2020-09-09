7328cb5d4a79fc2bfc19513e175f1f6985a900db
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 05:28:55 +0000

    created and migrated exercisereview and mealplanreview models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from products.models import Exercise, Mealplan
 
 # Create your models here.
+
+
+class ExerciseReview(models.Model):
+    # columns in the table
+    title = models.CharField(blank=False, max_length=255)
+    content = models.CharField(blank=False, max_length=255)
+    datetime = models.DateTimeField(auto_now=False)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+    # to make string
+    def __str__(self):
+        return self.title
+
+
+class MealplanReview(models.Model):
+    #columns in the table
+    title = models.CharField(blank=False, max_length=255)
+    content = models.CharField(blank=False, max_length=255)
+    datetime = models.DateTimeField(auto_now=False)
+    mealplan = models.ForeignKey(Mealplan, on_delete=models.CASCADE)
+
+    # to make string
+    def __str__(self):
+        return self.title

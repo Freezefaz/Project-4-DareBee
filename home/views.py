539019ed7c281c3ecbc991b7ed930b07a1cdeba9
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 03:12:58 +0000

    created welcome template and completed test

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome!")
+    # return HttpResponse("Welcome!")
+    return render(request, "home/welcome.template.html")

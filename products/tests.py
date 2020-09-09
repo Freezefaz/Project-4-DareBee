7af7dddfef3e3d7a9c61014755f9a774d7c2a903
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 09:10:50 +0000

    created test for product route

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+# for testing cases
+# 1st test will fail as no write up
+# Commit first
+# Then go settings.py to add url
+# then go urls to add path
+
+
 # Create your tests here.
+class ProductsTestView(TestCase):
+    def test_products_page(self):
+        # simulate going to products page
+        response = self.client.get("/")
+        # check if it goes to the exact page
+        self.assertEqual(response.status_code, 200)

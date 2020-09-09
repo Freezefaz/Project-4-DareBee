57a82bc790f345c9e52d6d4c307d70be5212a9ae
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 02:41:06 +0000

    created and tested home route

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTestViews(TestCase):
+    def test_home_page(self):
+        # simulate browswer going to URL
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)

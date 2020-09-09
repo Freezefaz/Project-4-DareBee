734a67e4564fb92b615ed5f9ec5e39e7720ffafe
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 07:57:57 +0000

    changed mealplanreview content to textfield

--- reviews/models.py
+++ reviews/models.py
@@ -21,7 +21,7 @@ class ExerciseReview(models.Model):
 class MealplanReview(models.Model):
     # columns in the table
     title = models.CharField(blank=False, max_length=255)
-    content = models.CharField(blank=False, max_length=255)
+    content = models.TextField(blank=False, max_length=255)
     datetime = models.DateTimeField(auto_now=True)
     mealplan = models.ForeignKey(Mealplan, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)

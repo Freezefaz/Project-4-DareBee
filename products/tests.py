5dc120002c52ac0732da6f8b2a2332186bf25f66
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-09 09:43:36 +0000

    added test criteria to include template

--- products/tests.py
+++ products/tests.py
@@ -14,3 +14,6 @@ class ProductsTestView(TestCase):
         response = self.client.get("/products/")
         # check if it goes to the exact page
         self.assertEqual(response.status_code, 200)
+        # check if it goes to the template
+        self.assertTemplateUsed(
+            response, "products/index_product.template.html")

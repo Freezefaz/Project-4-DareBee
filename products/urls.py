6db04fea80397a1de896645bd7862a3c5f18ad7e
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-10 05:11:35 +0000

    created and tested show exercise route url and view

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import products.views
 
 urlpatterns = [
-    path("", products.views.index, name="view_all_products")
+    path("", products.views.index, name="view_all_products"),
+    path("exercise/", products.views.show_exercise, name="view_all_exercise")
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-
+# show product pages
 def index(request):
     # return HttpResponse("Products")
     return render(request, "products/index_product.template.html")
+
+# show only all exercise page
+def show_exercise(request):
+    return HttpResponse("Exercise")
+    # return render(request, "products/index_product.template.html")

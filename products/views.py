1beb2353516f7d5818a3477f1cfc2f677acc7f35
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-10 06:39:54 +0000

    created create exercise template and completed test

--- products/views.py
+++ products/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, HttpResponse
+from .forms import ExerciseForm
 
 # Create your views here.
 
 # show product pages
+
+
 def index(request):
     # return HttpResponse("Products")
     return render(request, "products/index_product.template.html")
 
 # show only all exercise page
+
+
 def show_exercise(request):
     # return HttpResponse("Exercise")
     return render(request, "products/show_exercise.template.html")
 
 # create exercise
+
+
 def create_exercise(request):
-    return HttpResponse("Create Exercise")
+    create_form = ExerciseForm()
+    return render(request, "products/create_exercise.template.html", {
+        "form": create_form
+    })
+    # return HttpResponse("Create Exercise")

eae93158b28425c6a0ed2db61e78496ef3d4a335
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-11 03:15:32 +0000

    created form and template for create mealplan

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Exercise
+from .models import Exercise, Mealplan
 
 
 class ExerciseForm(forms.ModelForm):
     class Meta:
         model = Exercise
+        exclude = ("type",)
+
+class MealplanForm(forms.ModelForm):
+    class Meta:
+        model = Mealplan
         exclude = ("type",)

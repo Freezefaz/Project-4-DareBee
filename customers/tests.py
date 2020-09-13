a7fd1aaa263ba2434d19a0567008dd3d04708d09
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 07:56:00 +0000

    added template in create profile test

--- customers/tests.py
+++ customers/tests.py
@@ -3,7 +3,11 @@ from .models import Customer, Profile
 from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
 class ProfileTestView(TestCase):
     def test_create_profile_page(self):
         response = self.client.get("/profiles/create/")
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "customers/create_profile.template.html")

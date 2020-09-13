f47665413297718d1adc28b6a4dd2f1884727e0f
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 11:12:05 +0000

    created update profile test route

--- customers/tests.py
+++ customers/tests.py
@@ -17,3 +17,21 @@ class ProfileTestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(
             response, "customers/create_profile.template.html")
+
+    # To create a dummy user
+
+    def setUp(self):
+        self.customer = User(
+            username="test", email="test@test.com", password="test123")
+        self.customer.save()
+
+    def test_update_profile_page(self):
+        # to create a dummy profile to retrieve id
+        profile = Profile(customer=self.customer, first_name="Herb",
+                          last_name="Dean", dob="2020-10-09", height="182",
+                          weight="60", goals="best")
+        profile.save()
+        response = self.client.get(f"/profiles//update/{profile.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "profiles/update_profile.template.html")

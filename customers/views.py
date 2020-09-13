44dd66ffd9eb4df2c0d11070318e98df0d6e0c71
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 10:43:00 +0000

    updated views for create profile

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
 from .models import Customer, Profile
+from django.contrib.auth.models import User
 from .forms import ProfileForm
 
 import products.views
@@ -14,6 +15,20 @@ def show_profiles(request):
 
 def create_profile(request):
     # return HttpResponse("Your Profile")
+    if request.method == "POST":
+        create_form = ProfileForm(request.POST)
+        # check if created form have valid values
+        if create_form.is_valid():
+            profile_model = create_form.save(commit=False)
+            profile_model.customer = request.user
+            profile_model.save()
+            return redirect(reverse(show_profiles))
+        else:
+            # if does not have any valid values and re-render the form
+            return render(request, "customers/create_profile.template.html", {
+                "form": create_form
+            })
+    else:
         create_form = ProfileForm()
         return render(request, "customers/create_profile.template.html", {
             "form": create_form

4c2da6a5faa11886fb933fdd13aed3b76c2e6745
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 15:58:00 +0000

    created forms for exercise and mealplan reviews

---
+++ reviews/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import ExerciseReview, MealplanReview
+
+class ExerciseReviewForm(forms.ModelForm):
+    class Meta:
+        model = ExerciseReview
+        exclude = ('exercise', 'customer')
+
+class MealplanReviewForm(forms.ModelForm):
+    class Meta:
+        model = MealplanReview
+        exclude = ('exercise', 'customer')

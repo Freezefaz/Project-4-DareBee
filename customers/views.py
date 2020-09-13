2d240e4ca0e12c576a5f21c72f35932b59566b9f
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 08:33:18 +0000

    created create profile template and completed test

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-from .models import Customer, Profile
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from .models import Customer, Profile
+from .forms import ProfileForm
 
+import products.views
 # Create your views here.
 
 
+
 def create_profile(request):
-    return HttpResponse("Your Profile")
+    # return HttpResponse("Your Profile")
+        create_form = ProfileForm()
+        return render(request, "customers/create_profile.template.html", {
+            "form": create_form
+        })
+
+   

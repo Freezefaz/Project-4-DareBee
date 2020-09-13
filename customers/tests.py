17541862b26459018cd1d6f72dd110e58ea9ac6f
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-13 07:45:32 +0000

    created create profile route test

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Customer, Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestView(TestCase):
+    def test_create_profile_page(self):
+        response = self.client.get("/profiles/create/")
+        self.assertEqual(response.status_code, 200)

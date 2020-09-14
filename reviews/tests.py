8c4700b0894bacabf1ef1ed00218b83e05cdecd0
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-14 03:41:38 +0000

    created test for update exercise reviews

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import ExerciseReview, MealplanReview
+from products.models import Exercise, ExerciseType
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -15,8 +16,23 @@ class ExerciseReviewTestView(TestCase):
         self.assertTemplateUsed(
             response, "reviews/show_exercise_reviews.template.html")
 
+    # To create a dummy exercise type
+    def setUp(self):
+        self.exercise_type = ExerciseType(type="beginner")
+        self.exercise_type.save()
+
     def test_create_exercise_review_page(self):
-        response = self.client.get("/reviews/exercise/create/")
+        exercise = Exercise(title="Foundation", description="Easy",
+                            price="20", exercise_type=self.exercise_type)
+        response = self.client.get(f"/reviews/exercise/create/{exercise.id}")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(
             response, "customers/create_exercise_review.template.html")
+    
+    def test_update_exercise_review_page(self):
+        exercise = Exercise(title="Foundation", description="Easy",
+                            price="20", exercise_type=self.exercise_type)
+        response = self.client.get(f"/reviews/exercise/update/{exercise.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "customers/update_exercise_review.template.html")

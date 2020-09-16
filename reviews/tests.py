8858832fd8063fff4f2db75d1ea23064fd8cd3f2
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-16 06:08:40 +0000

    corrected test for create exercise and mealplan review

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import ExerciseReview, MealplanReview
-from products.models import Exercise, ExerciseType
+from products.models import Exercise, ExerciseType, Mealplan, MealplanType
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -21,32 +21,35 @@ class ExerciseReviewTestView(TestCase):
         self.exercise_type = ExerciseType(type="beginner")
         self.exercise_type.save()
 
-    # def test_create_exercise_review_page(self):
-    #     exercise = Exercise(title="Foundation", description="Easy",
-    #                         price="20", exercise_type=self.exercise_type)
-    #     response = self.client.get(f"/reviews/exercise/create/{exercise.id}")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(
-    #         response, "customers/create_exercise_review.template.html")
-    
-    # def test_update_exercise_review_page(self):
-    #     exercise = Exercise(title="Foundation", description="Easy",
-    #                         price="20", exercise_type=self.exercise_type)
-    #     response = self.client.get(f"/reviews/exercise/update/{exercise.id}")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(
-    #         response, "customers/update_exercise_review.template.html")
-
-class MealplanReviewTestView(TestCase):
-     # To create a dummy mealplan type
-      def setUp(self):
-        self.exercise_type = ExerciseType(type="beginner")
-        self.exercise_type.save()
-
-      def test_create_mealplan_review_page(self):
+    def test_create_exercise_review_page(self):
         exercise = Exercise(title="Foundation", description="Easy",
                             price="20", exercise_type=self.exercise_type)
+        exercise.save()
         response = self.client.get(f"/reviews/exercise/create/{exercise.id}")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(
-            response, "customers/create_exercise_review.template.html")
+            response, "reviews/create_exercise_review.template.html")
+
+    # def test_update_exercise_review_page(self):
+    #     exercise = Exercise(title="Foundation", description="Easy",
+    #                         price="20", exercise_type=self.exercise_type)
+    #     response = self.client.get(f"/reviews/exercise/{exercise.id}/update/{exercisereview.id}")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(
+    #         response, "reviews/update_exercise_review.template.html")
+
+
+class MealplanReviewTestView(TestCase):
+    # To create a dummy mealplan type
+    def setUp(self):
+        self.mealplan_type = MealplanType(type="muscle buidler")
+        self.mealplan_type.save()
+
+    def test_create_mealplan_review_page(self):
+        mealplan = Mealplan(title="Mass Effect", description="Big",
+                            price="20", mealplan_type=self.mealplan_type)
+        mealplan.save()
+        response = self.client.get(f"/reviews/mealplan/create/{mealplan.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "reviews/create_mealplan_review.template.html")

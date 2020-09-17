700ffb78483676e44af6829dd123ad5520c40188
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-17 08:55:55 +0000

    created add to cart views and url

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import cart.views
 
 urlpatterns = [
-    path("", cart.views.view_cart, name="view_cart")
+    path("", cart.views.view_cart, name="view_cart"),
+    path("add/<exercise_id>", add_to_cart, name="add_to_cart")
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Exercise
 
 # Create your views here.
 def view_cart(request):
@@ -7,3 +9,24 @@ def view_cart(request):
     return render(request, "cart/view_cart.template.html", {
         "shopping_cart": cart
     })
+
+def add_to_cart(request, exercise_id):
+    cart = request.session.get("shopping_cart", {})
+    # check if book is not in the cart, then add
+    if exercise_id not in cart:
+        exercise = get_object_or_404(Exercise, pk=exercise_id)
+        # book found add to cart
+        cart[exercise_id] = {
+            "id": exercise_id,
+            "title": exercise.title,
+            "price": exercise.price,
+            "qty": 1
+        }
+        # save the cart back to sessions
+        request.session["shopping_cart"] = cart
+        messages.success(request, "Exercise added to your cart!")
+        return redirect(reverse("products.views.show_exercise"))
+    else:
+        cart[exercise_id]["qty" += 1
+        request.session["shopping_cart"] = cart
+        return redirect(reverse("products.views.show_exercise"))

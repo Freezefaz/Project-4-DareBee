9a8606584e21db68c8f0b8588bb81db25c0e3ef0
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-17 07:24:36 +0000

    created test to view cart

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from products.models import Exercise, ExerciseType
 
 # Create your tests here.
+
+class TestCartView(TestCase):
+    def setUp(self):
+        self.exercise_type = ExerciseType(type="beginner")
+        self.exercise_type.save()
+    
+    def test_cart_view_page(self):
+        response = self.client.get("/cart/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, "cart/view_cart.template.html"
+        )

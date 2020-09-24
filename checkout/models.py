70cced60c0356c4bac04639b73b335aa944cb0f2
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-24 16:49:44 +0000

    created mealplan purchase model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Exercise_Purchase(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -13,11 +14,12 @@ class Exercise_Purchase(models.Model):
     def __str__(self):
         return f"Purchase made for id:{self.exercise.id} by {self.customer.username} on {self.purchase_date}"
 
-# class Mealplan_Purchase(models.Model):
-#     mealplan = models.ForeignKey(mealplan, on_delete=models.CASCADE)
-#     customer = models.ForeignKey(User, on_delete=models.CASCADE)
-#     purchase_date = models.DateTimeField(auto_now=True)
-#     quantity = models.PositiveIntegerField(blank=Fa)
 
-#     def __str__(self):
-#         return f"Purchase made for id:{self.exercise.id} by {self.customer.username} on {self.purchase_date}"
+class Mealplan_Purchase(models.Model):
+    mealplan = models.ForeignKey(Mealplan, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now=True)
+    price = models.PositiveIntegerField(blank=False)
+
+    def __str__(self):
+        return f"Purchase made for id:{self.exercise.id} by {self.customer.username} on {self.purchase_date}"

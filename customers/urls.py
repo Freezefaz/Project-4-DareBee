74ed44324db0c86f7506c2011954c0c970698871
Freezefaz <m.fareez.aziz@gmail.com>
2020-09-28 17:37:00 +0000

    created card class for exercise and styled it

--- customers/urls.py
+++ customers/urls.py
@@ -11,3 +11,15 @@ urlpatterns = [
     path("delete/<profile_id>", customers.views.delete_profile,
          name="delete_profile_route"),
 ]
+
+
+#   <!-- <ul>
+#             {% for each_exercise in all_exercises %}
+#             <li>
+#                 {% cloudinary each_exercise.cover class="exercise_cover"%}
+#                 <a href="{% url 'exercise_details_route' exercise_id=each_exercise.id %}">{{each_exercise.title}}</a> 
+#                 <a class="btn btn-primary" href="{% url 'update_exercise_route' exercise_id=each_exercise.id%}">Edit</a>
+#                 <a class="btn btn-danger" href="{% url 'delete_exercise_route' exercise_id=each_exercise.id%}">Delete</a>
+#                 <a class="btn btn-dark" href="{% url 'add_to_exercise_cart_route' exercise_id=each_exercise.id%}">Add to Cart</a>
+#             </li>
+#         </ul> -->

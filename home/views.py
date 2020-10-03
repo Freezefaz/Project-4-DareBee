63dd66396c8bf51a2b443f6115df915562ab48a1
Freezefaz <m.fareez.aziz@gmail.com>
2020-10-03 12:27:11 +0000

    remove unnecessary code from home app

--- home/views.py
+++ home/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    # return HttpResponse("Welcome!")
     return render(request, "home/welcome.template.html")

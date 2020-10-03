e62f3d128a9de7d54f0d87124ac23236c1de9fdf
Freezefaz <m.fareez.aziz@gmail.com>
2020-10-03 13:35:16 +0000

    removed url for update and delete reviews as only create is allowed

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,25 +2,9 @@ from django.urls import path
 import reviews.views
 
 urlpatterns = [
-    path("exercise/", reviews.views.show_exercise_reviews,
-         name="view_all_exercise_review_route"),
     path("exercise/create/<exercise_id>", reviews.views.create_exercise_review,
          name="create_exercise_review_route"),
-    # path("exercise/<exercise_id>/update/<exercisereview_id>",
-    #      reviews.views.update_exercise_review,
-    #      name="update_exercise_review_route"),
-    path("exercise/update/<exercisereview_id>",
-         reviews.views.update_exercise_review,
-         name="update_exercise_review_route"),
-    path("exercise/delete/<exercisereview_id>",
-         reviews.views.delete_exercise_review,
-         name="delete_exercise_review_route"),
     path("mealplan/create/<mealplan_id>", reviews.views.create_mealplan_review,
          name="create_mealplan_review_route"),
-    path("mealplan/update/<mealplanreview_id>",
-         reviews.views.update_mealplan_review,
-         name="update_mealplan_review_route"),
-    path("mealplan/delete/<mealplanreview_id>",
-         reviews.views.delete_mealplan_review,
-         name="delete_mealplan_review_route")
+
 ]

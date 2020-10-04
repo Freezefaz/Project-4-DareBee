7b2260ce5a0ddefb03d26e05a85d1818dab0feb9
Freezefaz <m.fareez.aziz@gmail.com>
2020-10-04 03:35:30 +0000

    turned debug false

--- DareBeeProject/settings.py
+++ DareBeeProject/settings.py
@@ -27,7 +27,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "fa-darebee.herokuapp.com"]
 

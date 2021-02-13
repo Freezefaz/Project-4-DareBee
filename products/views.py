1297a00a4c1e3a91b7815ba6acd1b0e70fe18015
Freezefaz <m.fareez.aziz@gmail.com>
2021-02-13 07:10:45 +0000

    corrected staff required import

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, HttpResponse, redirect, reverse, get_object
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import staff_member_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 
